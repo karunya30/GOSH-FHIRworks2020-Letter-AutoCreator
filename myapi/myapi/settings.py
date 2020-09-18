56da38fb0f00602108e209338861801c2e3b0354
karunya30 <zcabsel@ucl.ac.uk>
2020-09-18 15:23:46 +0100

    removed secret key- final version

--- myapi/myapi/settings.py
+++ myapi/myapi/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'npq4i@!egfy207$7md+!n#n0!tyv((pps4pz)b$9g-v#zj7n7+'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'bao=g2$sa$5*0m_6z23evf@(q8dk!8ot+gs@68+ebdl(bd7q1&'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
